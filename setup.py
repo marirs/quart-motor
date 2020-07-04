e0923eb2cd5ad43283055b25a8957d3706a5f6ca
Sriram G <sgp@alphasun.local>
2020-07-04 19:38:17 +0530

    housekeeping

--- setup.py
+++ setup.py
@@ -9,20 +9,25 @@ Source code is hosted on `GitHub <https://github.com/marirs/quart-motor>`_.
 Contributions are welcome!
 """
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name="Quart-Motor",
+    version='2.4.0',
     url="https://www.github.com/quart-motor/",
     download_url="https://www.github.com/quart-motor/tags",
     license="BSD",
     author="Sriram G",
     author_email="marirs@gmail.com",
     description="Motor support for Quart applications",
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     zip_safe=False,
     platforms="any",
-    packages=find_packages(),
+    packages=['quart_motor'],
     install_requires=[
         "Quart>=0.12.0",
         "motor>=2.1.0"
@@ -31,14 +36,12 @@ setup(
     ],
     classifiers=[
         "Environment :: Web Environment",
-        "Framework :: Quart",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
